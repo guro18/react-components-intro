cdbf8dc076817d40c13d5d2a64ba92a373f09105
guro18 <rollgustav95@gmail.com>
2024-02-21 10:49:58 +0100

    completed

--- src/main.jsx
+++ src/main.jsx
@@ -7,3 +7,22 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
+function Main () {
+  return (
+    <main>
+      <img
+        className="logo"
+        src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+        alt="Google logo"
+      />
+      <input className="search-bar" type="search" />
+      <div className="buttons">
+        <button>Google Search</button>
+        <button>Feeling Lucky</button>
+      </div>
+    </main>
+  )
+}
+
+export default Main;
